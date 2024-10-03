948e96b58e6122875a0f0358a6dad33f30963c93
Aleckqhie <reckalecks@gmail.com>
2024-10-03 07:44:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# macodess" 
---
+++ variables and datatypess. c.c
@@ -0,0 +1,30 @@
+/* programme for variables and data types 
+the code creator is Alex Mwangi Ndaguri
+registration number CT101/G/23713/24
+ preprocessor directive scanf(), printf () 
+ */
+#include<stdio.h>
+int main() {
+//declaration and initialization
+char a ='H';
+char name[]={};
+int age;
+float salary;
+double budget;
+printf("enter a character");
+scanf("%c",&a);
+printf("the character is%c\n",a);
+printf("enter your name:") ;
+scanf("%s",&name);
+printf("your name is%s\n ",name);
+printf("enter your age ");
+scanf("%d",&age);
+printf("your age is%d\n",age);
+printf("enter your salary");
+scanf("%f",&salary);
+printf("your salary is%f\n",salary);
+printf("enter your budget");
+scanf("%lf",&budget);
+printf("your budget is%lf\n",budget);
+return 0;
+}
